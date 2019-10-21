0855a83ac0a27243395c4b609d0a8a0b3eb474f7
PatrickRogg <Patrick.Rogg@adesso.de>
2019-10-21 14:02:08 +0200

    Cleaned up unnecessary attributes

--- book-service/src/main/java/com/example/libraryservice/entity/Book.java
+++ book-service/src/main/java/com/example/libraryservice/entity/Book.java
@@ -12,7 +12,7 @@ public class Book {
   @NotNull private String author;
   @NotNull private String title;
 
-  public Book(String isbn, String author, String title, String publisher) {
+  public Book(String isbn, String author, String title) {
     this.isbn = isbn;
     this.author = author;
     this.title = title;
